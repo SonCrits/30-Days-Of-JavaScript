f697e7157d4b408323e0ca7fd76c5041b429c684
Tran Truong Son <truongson290893@gmail.com>
2023-12-08 18:00:49 +0700

    [ADD] 01_day_my_exersice

---
+++ 01_Day_Introduction/01_day_my_excersice/data-types.js
@@ -0,0 +1,12 @@
+let firstName = 'Son'
+let lastName = 'Tran'
+let country = 'Vietnam'
+let city = 'Hai Duong'
+let age = 30
+let isMarried = true
+
+const PI = Math.PI
+const gravity = 9.81
+const boilingPoint = 100
+
+let name = 'Son', job = 'Developer', live = 'Hai Duong'
---
+++ 01_Day_Introduction/01_day_my_excersice/main.js
@@ -0,0 +1,3 @@
+console.log(firstName, lastName, country, city, age, isMarried)
+console.log(PI, gravity, boilingPoint)
+console.log(name, job, 'in', live)

73605326029cff0f44330c087851227b0cdbb8bc
Tran Truong Son <truongson290893@gmail.com>
2023-12-21 15:39:59 +0700

    [ADD] 03_day_excersice

---
+++ 03_Day_Booleans_operators_date/day_03_my_excersice/level_2.js
@@ -0,0 +1,9 @@
+let base;
+let height;
+
+base = prompt("Enter base: ");
+height = prompt("Enter height: ");
+
+let area = base * height / 2;
+
+alert("Area: " + area)
---
+++ 03_Day_Booleans_operators_date/day_03_my_excersice/level_3.js
@@ -0,0 +1,1 @@
+console.log(year + " -", date.getMonth() + 1 + " - " + date.getDate())
